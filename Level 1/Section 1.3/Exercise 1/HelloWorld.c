05afa757f625fc53b96d4dd23a1509fb21eb9115
bz866 <bz866@nyu.edu>
2020-07-01 21:22:35 -0400

    level 1 E 1.3 Created

---
+++ Level 1/Section 1.3/Exercise 1/HelloWorld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main() {
+    /*
+    C-program that prints a message 
+    */
+   printf("My first C-program\n");
+   printf("is a fact!\n");
+   printf("Good, isn't it?");
+   
+}
